e807d0338587db2051bc51d76124f05b636f5f8d
Jacy Wang <jacywj@gmail.com>
2015-05-15 13:06:45 -0700

    Initialize repo

---
+++ FingerPaint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FingerPaint
+//
+//  Created by JIAN WANG on 5/15/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
